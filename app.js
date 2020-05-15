950f3472c14248bae4d7bc34c1fe51f4a3f9b9e7
guillaume <guillaumechaulet@hotmail.fr>
2020-05-15 10:22:50 +0200

    log configuration

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame () {
@@ -11,9 +12,13 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
+        //checl if player won at first or none
         attack () {
             this.monsterHealth -= this.calculateDamage(3,10);
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for ' + damage
+            });
             if(this.checkWin()) {
                 return;
             }
@@ -22,7 +27,10 @@ new Vue({
         },
         specialAttack () {
             this.monsterHealth -= this.calculateDamage(10,20);
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster hard for ' + damage
+            });
             if(this.checkWin()) {
                 return;
             }
@@ -30,24 +38,35 @@ new Vue({
             this.monsterAttacks();
            
         },
+        //give point to player
         heal () {
             if (this.playerHealth <= 90) {
                 this.playerHealth += 10;
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttacks();
         },
         giveUp () {
-
+            this.gameIsRunning = false;
         },
         monsterAttacks () {
             this.playerHealth -= this.calculateDamage(5,12);
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Monster hits player for ' + damage
+            });
         },
+        //function which calculate parameters for damages
         calculateDamage (min, max) {
             return damage = Math.max(Math.floor(Math.random() * max) + 1, min);
         },
+        //Restart game or not
         checkWin () {
             if (this.monsterHealth <= 0) {
                 if (confirm('You won, New game ?')){
